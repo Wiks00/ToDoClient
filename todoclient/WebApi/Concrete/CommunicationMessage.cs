7342ac053fbc0d4bc5a1336bd6f325360be77efb
Илья <wiks30@gmail.com>
2017-04-11 18:04:04 +0300

    Attempts to create logic

---
+++ todoclient/WebApi/Concrete/CommunicationMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi.Models;
+
+namespace WebApi.Concrete
+{
+    public enum Operation
+    {
+        Add = 0,
+        Delete = 1,
+        Update = 2
+    }
+
+    public class CommunicationMessage
+    {
+        public Operation Operation { get; set; }
+
+        public ToDoItemModel ToDoItem { get; set; }
+
+    }
+}
