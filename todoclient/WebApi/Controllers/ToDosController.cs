d8f0959c35ace9e20b3cbac029cc100753677933
Илья <wiks30@gmail.com>
2017-04-11 22:48:37 +0300

    fix

---
+++ todoclient/WebApi/Controllers/ToDosController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApi.Concrete;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    public class ToDosController : ApiController
+    {
+        private SyncService GetSyncService { get; } = new SyncService();
+
+        public async Task<IList<ToDoItemModel>> Get(int userId)
+        {
+            return await GetSyncService.GetToDoItemsAsync(userId);
+        }
+
+        public void Put(ToDoItemModel todo)
+        {
+            GetSyncService.UpdateToDoItem(todo);
+        }
+
+        public void Delete(ToDoItemModel todo)
+        {
+            GetSyncService.DeleteToDoItem(todo);
+        }
+
+        public void Post(ToDoItemModel todo)
+        {
+            GetSyncService.AddToDoItem(todo);
+        }
+
+        
+
+    }
+}
