4ee49e5f1ef9835991fcd835a1e4af5ad05e1c10
Илья <wiks30@gmail.com>
2017-04-11 20:13:53 +0300

    Sync fichers added . Not tested

---
+++ todoclient/WebApi/Helpers/ToDoItemHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi.Models;
+
+namespace WebApi.Helpers
+{
+    public static class ToDoItemHelper
+    {
+        public static int GetId(this ToDoItemModel toDoItem)
+            => int.Parse(
+                    toDoItem.Name.Substring(
+                        toDoItem.Name.LastIndexOf(",", StringComparison.InvariantCultureIgnoreCase) + 1));
+    }
+}
