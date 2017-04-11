c0b06031e7ef2cf9a57c3328714976a4126bb6c5
Dmitry Struts <fakekooler@gmail.com>
2017-04-11 17:39:54 +0300

    ORM init

---
+++ todoclient/ORM/ToDoContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ORM
+{
+    public class ToDoContext:DbContext
+    {
+        public ToDoContext()
+        {
+
+        }
+
+        public DbSet<ToDoTask> ToDoTasks { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
