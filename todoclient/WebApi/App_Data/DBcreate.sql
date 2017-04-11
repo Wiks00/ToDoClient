fe9b20c4b4c22f12fbb73f79882c8039eff91eb9
Илья <wiks30@gmail.com>
2017-04-11 22:00:35 +0300

    Orm added + rwlock changed to blockingColl . Not tested

---
+++ todoclient/WebApi/App_Data/DBcreate.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Users (
+	id INT NOT NULL,
+	username varchar(1000) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE ToDoTask (
+	Id  INT NOT NULL,
+	UserId INT NOT NULL,
+	Name varchar(100) NOT NULL,
+	IsCompleted BIT NOT NULL,
+	PRIMARY KEY (Id )
+);
+
+ALTER TABLE ToDoTask ADD CONSTRAINT ToDoTask_fk0 FOREIGN KEY (UserId) REFERENCES Users(id);
