d9b1f4e981b3ff962e22b6c2df91b6bc90dea1e9
Илья <wiks30@gmail.com>
2017-04-11 20:13:53 +0300

    Sync fichers added . Not tested

--- todoclient/WebApi/Models/ToDoItemModel.cs
+++ todoclient/WebApi/Models/ToDoItemModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebApi.Helpers;
 
 namespace WebApi.Models
 {
@@ -39,5 +40,12 @@ namespace WebApi.Models
         /// The name.
         /// </value>
         public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            //ToDo: change
+
+            return this.GetId() == (obj as ToDoItemModel).GetId();
+        }
     }
 }
